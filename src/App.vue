ac7bfb37ab601431742c5d00bc27e2ac6460c9cf
VKohai <vkohai.it.business@gmail.com>
2025-09-03 16:25:02 +0300

    Test init data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+const telegramInitData: string = window.Telegram.WebApp.initData || 'Not avaliable'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="telegramInitData" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
